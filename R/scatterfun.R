d24269e770e04f7709bad2017598e5943ba6426a
Erin Boyle Anderson <erinboyleanderson@gmail.com>
2018-12-04 19:10:20 -0600

    updated scatter to calculate it so that it is calculated per embryo

--- R/scatterfun.R
+++ R/scatterfun.R
@@ -1,6 +1,10 @@
 #' Function for determining scatter
 #'
 #' This function calulcates scatter, which is one measure of how far apart embryos are over a time series. IT is essentially sd[t=n]/sd[t=1]
+#' Note: scatterX is AP scatter, scatter Y is ML scatter PER EMBRYO
+#' mscatterX is the mean scatterX and mscatterY is the MEAN scatter Y over all embryos.
+#' seX is AP standard error, seY is ML standard error
+#'
 #' @param df time series dataframe
 #' @export
 
@@ -8,16 +12,23 @@
 
 scatterfun<-function(df){# would it be different if I grouped by embryo as well?
   a<-df%>%
-    group_by(genotype,Time)%>%
-    mutate(sd(X),sd(Y))%>%
+    group_by(Embryo,genotype,Time)%>% # calculate scatter for each embryo in order to calc sd
+    mutate(sdX=sd(X),sdY=sd(Y))%>%
     ungroup%>%
-    select(Time, 'sd(X)','sd(Y)')%>%
-    distinct%>% #removes duplicates
-    rename(sdX="sd(X)",sdY="sd(Y)")
-  a<-a%>%
-    mutate(sdX/a$sdX[1],sdY/a$sdY[1])%>%
-    rename(scatterX=`sdX/a$sdX[1]`,scatterY=`sdY/a$sdY[1]`)%>%
-    mutate(seX=sd(scatterX)/sqrt(length(scatterX)),
+    select(Time, sdX,sdY, genotype, Embryo)%>%
+    distinct # because there are many tracks for embryo, so we only need one set
+  b<-filter(a, Time==1)%>%
+    rename(sdX1=sdX,sdY1=sdY)%>%
+    select(Embryo, genotype, sdX1,sdY1)
+  a<-right_join(a,b)%>%
+    group_by(genotype, Time, Embryo)%>%
+    mutate(scatterX=sdX/sdX1,scatterY=sdY/sdY1)%>%
+    ungroup%>%
+    #select(genotype, Time, scatterX, scatterY)%>%
+    group_by(genotype, Time)%>%
+    mutate(mscatterX=mean(scatterX), # mean values of scatterX
+           mscatterY=mean(scatterY), # mean values of scatterY
+           seX=sd(scatterX)/sqrt(length(scatterX)),
            seY=sd(scatterY)/sqrt(length(scatterY)))
   return(a)
 }
